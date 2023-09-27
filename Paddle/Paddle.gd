a2b900fa7a39871dd20468cb34044e4de300c6f2
aonkd <a.j.lambert23@gmail.com>
2023-09-27 18:22:14 -0400

    completes exercise

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -12,10 +12,14 @@ func _ready():
 func _physics_process(_delta):
 	target.x = clamp(target.x, 0, Global.VP.x - 2*width)
 	position = target
+	if $highlight.modulate.a > 0:
+		$highlight.modulate.a -= decay
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		target.x += event.relative.x
 
 func hit():
+	$highlight.modulate.a = 1
+	$confetti.emitting = true
 	pass
--- Wall/Wall.gd
+++ Wall/Wall.gd
@@ -6,7 +6,13 @@ func _ready():
 	pass
 
 func _physics_process(_delta):
+	if $ColorRect.color.s > 0:
+		$ColorRect.color.s -= decay
+	if $ColorRect.color.v < 1:
+		$ColorRect.color.v += decay
 	pass
+	
 
 func hit():
+	$ColorRect.color = Color8(201,42,42,255)
 	pass
